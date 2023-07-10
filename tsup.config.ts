596c082e17c17ac84df815f20f6969102ee019e4
luxluth <delphin.blehoussi93@gmail.com>
2023-07-10 10:45:29 +0200

    Update tsup

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entryPoints: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    clean: true,
+    splitting: true,
+    platform: 'browser',
+    target: 'esnext',
+})
