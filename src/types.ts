1d70918bcbdcc87bcc6aabe60fe9406546ebed46
luxluth <delphin.blehoussi93@gmail.com>
2024-08-29 16:01:21 +0200

    Removing ASSAnimation namespace

--- src/types.ts
+++ src/types.ts
@@ -96,52 +96,6 @@ export type FontTransfomation = {
 
 export type Tag = { [K in keyof ParsedTag]: ParsedTag[K] };
 
-export declare namespace ASSAnimation {
-  export type Fade =
-    | {
-        name: 'fad';
-        /**
-         * The values of the fade animation
-         * `\fad(<fadein>,<fadeout>)` or `\fade(<a1>,<a2>,<a3>,<t1>,<t2>,<t3>,<t4>)`
-         */
-        values: FadValues;
-      }
-    | {
-        name: 'fade';
-        values: FadeValues;
-      };
-
-  export type FadeValues = [number, number, number, number, number, number, number];
-  export type FadValues = [number, number];
-
-  export type MoveValues = MoveValuesSimple | MoveValuesComplex;
-
-  export type MoveValuesSimple = [number, number, number, number];
-  export type MoveValuesComplex = [number, number, number, number, number, number];
-
-  export type Move = {
-    name: 'move';
-    /**
-     * The values of the move animation
-     * `\move(<x1>,<y1>,<x2>,<y2>)` or `\move(<x1>,<y1>,<x2>,<y2>,<t1>,<t2>)`
-     */
-    values: MoveValues;
-  };
-
-  export type Org = {
-    name: 'org';
-    /**
-     * The values of the Rotation origin animation
-     * `\org(<x>,<y>)`
-     */
-    values: OrgValues;
-  };
-
-  export type OrgValues = [number, number];
-
-  export type Animation = Fade | Move | Org;
-}
-
 export type Override = {
   dialogue: Dialogue;
   style: CompiledASSStyle;
